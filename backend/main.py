c97268962a2c72f2fb977061b1b1da30428d9248
Manish4325 <manishroyal@rbihub.in>
2026-01-22 15:47:04 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,9 @@
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# @app.get("/")
-# def health():
-#     return {"status": "Backend is running"}
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from schemas import ChatRequest
 from llm import stream_groq
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.post("/chat")
 def chat(req: ChatRequest):
@@ -27,7 +11,3 @@ def chat(req: ChatRequest):
         stream_groq(req.message),
         media_type="text/plain"
     )
-
-@app.get("/")
-def health():
-    return {"status": "Backend running"}
